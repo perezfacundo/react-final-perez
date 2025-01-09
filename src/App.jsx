4daf9149325bfaac423b07d07d4d9f20cfdabec6
Facundo <perezfaq98@gmail.com>
2025-01-09 12:41:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  
+
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
